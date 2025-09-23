35acbb745a1f440836379e2048aaaa38514b7b02
moonicecreamstudio <moonicecreamstudio@gmail.com>
2025-09-23 15:13:06 -0400

    Added highlighted text capabilities via hover

---
+++ UI Exploration/Assets/Scripts/UIHighlighter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+using static System.Net.Mime.MediaTypeNames;
+
+public class UIHighlighter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public TextMeshProUGUI _text;
+    private Color _startingColor;
+    public Color _highlightedColor;
+
+    private void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        Color startingColor = _text.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _text.color = Color.yellow;
+        _text.fontSize = 100;
+        Debug.Log("Mouse on.");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _text.color = Color.white;
+        _text.fontSize = 72;
+        Debug.Log("Mouse off.");
+    }
+
+}
